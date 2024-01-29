65580eaeeccdeec084c81c997aa228fa77f140e7
Alpesh Solanki <76895027+alpeshSolanki7271@users.noreply.github.com>
2024-01-29 12:21:48 +0530

    Personal Info

---
+++ main.js
@@ -0,0 +1,35 @@
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () =>{
+
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+}
+
+const sections = document.querySelectorAll("section");
+// header nav a header ul li a
+const navLi = document.querySelectorAll("header ul li a");
+// const navLi = document.querySelectorAll("nav .container ul li");
+window.addEventListener("scroll", () => {
+  let current = "";
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+    if (pageYOffset >= sectionTop - sectionHeight / 3) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLi.forEach((li) => {
+    li.classList.remove("active");
+    if (li.classList.contains(current)) {
+      li.classList.add("active");
+    }
+  });
+});
